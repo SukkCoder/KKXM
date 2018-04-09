38c12a347186a5f071ad97245c916da23444f4d4
WANAN_SKK <wanan@wanan-skkdemacbook-pro.local>
2018-04-09 16:38:29 +0800

    Initial Commit

---
+++ KKXM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KKXM
+//
+//  Created by WANAN_SKK on 2018/4/9.
+//  Copyright © 2018年 KK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
