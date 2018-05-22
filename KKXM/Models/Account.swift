9c200a184a127b857c80966c04d6663a4279a1de
SukkCoder <sukkcoder@icloud.com>
2018-05-22 10:44:29 +0800

    add

---
+++ KKXM/Models/Account.swift
@@ -0,0 +1,20 @@
+//
+//  Account.swift
+//  Key
+//
+//  Created by WANAN_SKK on 2018/3/20.
+//  Copyright © 2018年 WANAN_SKK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Account {
+    
+    var userId: Int?
+    var username: String
+    var password: String
+    var icon: UIImage!
+    var name: String
+    var notes: String
+}
---
+++ KKXM/Models/KK_Tool.swift
@@ -0,0 +1,16 @@
+//
+//  KK_Tool.swift
+//  KKXM
+//
+//  Created by WANAN_SKK on 2018/4/9.
+//  Copyright © 2018年 KK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct KK_Tool {
+    var name: String!
+    var icon: UIImage!
+    var desc: String!
+}
